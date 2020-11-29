bee5eeb9a5e75a1ad7b4b4bd276d9e10539d9c6c
darragh2 <72267924+darragh2@users.noreply.github.com>
2020-11-29 20:43:36 +0000

    Ui added

---
+++ Obstacle mania/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public string URL = "https://github.com/darragh2/Game-alpha";
+
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void GoToLevel2()
+    {
+        SceneManager.LoadScene("level2");
+    }
+
+    public void GoToLevel3()
+    {
+        SceneManager.LoadScene("level3");
+    }
+
+    public void LoadURL()
+    {
+        Application.OpenURL(URL);
+    }
+
+}
